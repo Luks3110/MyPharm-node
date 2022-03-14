b756273cc68e9e0501b077c817cd8092eff9fa00
Luks3110 <lucas_bugado@hotmail.com>
2022-03-14 01:39:57 -0300

    atlas

--- src/app.js
+++ src/app.js
@@ -5,8 +5,9 @@ const mongoose = require('mongoose')
 const app = express()
 const cookieParser = require('cookie-parser')
 const { checkToken } = require('./middleware/authMiddleware')
+const uri = process.env.MONGODB_URI
 
-mongoose.connect('mongodb://localhost:27017/mypharm_db')
+mongoose.connect(uri)
 .then(() => console.log('Connected to MongoDB...'))
 .catch((err) => console.error('Could not connect to MongoDB...', err))
 
